179a16b6c07d17a4f5ee4a01da88acc667360b24
Archimeister757 <74320080+Archimeister757@users.noreply.github.com>
2021-12-13 11:52:23 +0300

    test

--- src/js/script.js
+++ src/js/script.js
@@ -14,26 +14,28 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('promo__menu_active');
         });
     });
-});
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 500,
-        infinite: true,
-        variableWidth: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left_arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right_arrow.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '20px',
+    $(document).ready(function(){
+        $('.carousel__inner').slick({
+            speed: 500,
+            infinite: true,
+            variableWidth: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left_arrow.png"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right_arrow.png"></button>',
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '20px',
+                    }
                 }
-            }
-        ]
+            ]
+        });
     });
 });
+
+
